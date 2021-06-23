b90ab67116f989f620e11976ffa35e2b0640750c
xq0107 <3425126218@qq.com>
2021-06-23 16:27:57 +0800

    aaa

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import *
+
+app = Create_app()
+
+
+if __name__ == '__main__':
+    app.run(port='777',Debug=True)
---
+++ apps/__init__.py
@@ -0,0 +1,7 @@
+from flask import *
+
+def Create_app():
+    app = Flask(__name__)
+    app.register_blueprint
+
+    return app
