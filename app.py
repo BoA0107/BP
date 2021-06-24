cd9ec71586c7f9d6073e9f5677bd07c6ce613fbe
xq0107 <3425126218@qq.com>
2021-06-24 17:11:51 +0800

    aaa

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-from flask import *
-from read_doc import *
-
-F_home = r'doc/home.xlsx'
-F_BCA = r'doc/BCA.xlsx'
-F_SQL = r'doc/SQL.xlsx'
-
-F_S_links = r'links'
-F_S_deploy_plan = r'deploy_plan'
-F_S_level_info = r'level_info'
-
+from func.func_home import *
+from func.func_BCA import *
 
 app = Flask(__name__)
 
@@ -25,22 +16,12 @@ def index():
 
 @app.route('/home')
 def home():
-    # plan_info
-    plan_info = read_doc(filename=F_home, sheetname=F_S_deploy_plan, max_line=16)
-
-    # links
-    links = read_doc(filename=F_home, sheetname=F_S_links)
-    links_dct = make_dct(links)
-
-    # career_level
-    career_level = read_doc(filename=F_home, sheetname=F_S_level_info)
-
     return render_template("home.html", plan_info=plan_info, links=links_dct, career_level=career_level)
 
 
 @app.route('/BCA')
 def BCA():
-    return render_template('BCA.html')
+    return render_template('BCA.html',UAT_BC=UAT_BC)
 
 
 @app.route('/SQL')
---
+++ func/info.py
@@ -0,0 +1,13 @@
+# all xlsx
+F_home = r'doc/home.xlsx'
+F_BCA = r'doc/BCA.xlsx'
+F_SQL = r'doc/SQL.xlsx'
+
+# sheet for home
+Home_links = r'links'
+Home_deploy_plan = r'deploy_plan'
+Home_level_info = r'level_info'
+
+# sheet for BCA
+BCA_UAT_BC=r'UAT_BC'
+
