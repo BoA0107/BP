86b419b259c85152e48779d7ca94bbd1a00ccf7e
xq0107 <3425126218@qq.com>
2021-06-24 16:31:38 +0800

    aaa

--- app.py
+++ app.py
@@ -1,8 +1,36 @@
 from flask import *
 
-from apps import create_app
+app = Flask(__name__)
 
-app = create_app()
+
+@app.route('/')
+def show():
+    return render_template('show.html')
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/BCA')
+def BCA():
+    return render_template('BCA.html')
+
+
+@app.route('/SQL')
+def SQL():
+    return render_template('SQL.html')
+
+@app.route('/deploy')
+def deploy():
+    return render_template('deploy.html')
+
 
 if __name__ =="__main__":
     app.run(port=7777,debug=True)
