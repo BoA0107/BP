a84f0e99e0db62afcb9df4d6fa3fd18bdfae150f
xq0107 <3425126218@qq.com>
2021-06-28 17:26:15 +0800

    aaa

---
+++ func/func_deploy.py
@@ -0,0 +1,5 @@
+from read_doc import *
+from func.info import *
+
+answer = read_doc(filename=F_deploy, sheetname=answer)
+b_version =read_doc(filename=F_deploy, sheetname=branch_version)
--- func/info.py
+++ func/info.py
@@ -1,15 +1,24 @@
+# -*- coding: UTF-8 -*-
+
 # all xlsx
 F_home = r'doc/home.xlsx'
 F_BCA = r'doc/BCA.xlsx'
 F_SQL = r'doc/SQL.xlsx'
+F_deploy = r'doc/deploy.xlsx'
 
 # sheet for home
 Home_links = r'links'
 Home_deploy_plan = r'deploy_plan'
 Home_level_info = r'level_info'
+Home_normal_info = r'normal_info'
+Home_others = r'others'
 
 # sheet for BCA
 BCA_UAT_BC = r'UAT_BC'
+
+# deploy
+answer = r'answer'
+branch_version = r'b_version'
 
 # SQL
 DIQ = r'doc/SQL/DIQ.txt'
@@ -19,3 +28,6 @@ NSD = r'doc/SQL/NSD.txt'
 batch = r'doc/SQL/batch.txt'
 new_BC = r'doc/SQL/new_BC.txt'
 change_diq = r'doc/SQL/change_diq.txt'
+DBrestore = r'doc/SQL/dbrestore.txt'
+DBbackup = r'doc/SQL/dbbackup.txt'
+ServiceBroker = r'doc/SQL/service.txt'
